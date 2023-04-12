7d235c7cc4e6254fcddf3eeae5380ed4b15f29d6
seventinylantemona17 <lseventiny@gmail.com>
2023-04-12 13:00:55 +0700

    tugas7-8

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Footer = () => {
+    return <div className="py-4 text-center text-white bg-primary "> © 2023 seventinylantemona all right reserved</div>;
+};
+export default Footer;
